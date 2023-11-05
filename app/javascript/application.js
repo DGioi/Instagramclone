5d5df50237cdce909af25834855f85634df912df
Duong Gioi <118875482+DGioi@users.noreply.github.com>
2023-11-05 23:09:45 +0700

    hic preview images error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,28 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+
+
+document.addEventListener("turbolinks:load", function() {
+    var fileInput = document.querySelector("#image-upload");
+    var previewContainer = document.querySelector("#image-preview");
+  
+    fileInput.addEventListener("change", function(event) {
+      previewContainer.innerHTML = "";
+  
+      var files = event.target.files;
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var reader = new FileReader();
+  
+        reader.onload = function(e) {
+          var image = document.createElement("img");
+          image.src = e.target.result;
+          image.classList.add("preview-image");
+          previewContainer.appendChild(image);
+        };
+  
+        reader.readAsDataURL(file);
+      }
+    });
+  });
